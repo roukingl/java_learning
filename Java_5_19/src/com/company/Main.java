f7d0e21d89245b40c231b0696a5195e438e6c170
gitee_drh <2062806593@qq.com>
2022-05-19 20:20:43 +0800

    5_19的Java练习

---
+++ Java_5_19/src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.Locale;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String str = "   has    exfoliates\n";
+
+        System.out.println(str);
+        System.out.println(str.toUpperCase());
+        System.out.println(str.trim());
+        System.out.println(str.toUpperCase().trim());
+    }
+}
