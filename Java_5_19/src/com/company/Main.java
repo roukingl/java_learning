5c0a5b75081d9d20b7e35e9524bea77916d6bc1c
gitee_drh <2062806593@qq.com>
2022-05-19 20:37:44 +0800

    5_19的Java练习

--- Java_5_19/src/com/company/Main.java
+++ Java_5_19/src/com/company/Main.java
@@ -5,10 +5,26 @@ import java.util.Locale;
 public class Main {
 
     public static void main(String[] args) {
-        short a = 128;
-        byte b = (byte)a;
-        System.out.println(b);
-        System.out.println(a);
+
+        //int a = (int)934.0;
+
+//        byte a = 10;
+//        int b = -a;
+        double f = 100;
+        System.out.println(f);
+
+
+
+        /*int a = 3;
+        byte b = a;
+        System.out.println(b);*/
+        /*//\"hello\"
+
+        System.out.println("\\\"hello\\\"");*/
+//        short a = 128;
+//        byte b = (byte)a;
+//        System.out.println(b);
+//        System.out.println(a);
 
        /* String str = "   has    exfoliates\n";
 
