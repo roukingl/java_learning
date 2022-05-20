626bce4c8589cd387b8df4d89b904929b0fe4e06
gitee_drh <2062806593@qq.com>
2022-05-20 17:37:37 +0800

    5_20的代码

---
+++ Java_5_20/src/com/company/Main.java
@@ -0,0 +1,70 @@
+package com.company;
+
+import static java.lang.Math.abs;
+import static java.lang.Math.log;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(100);
+
+    }
+}
+/*
+//        System.out.println(Math.random());
+        int a = 10;
+        int b = 20;
+        System.out.println(sum(20, 10));
+
+        public static int sum(int number_a, int number_b){
+            return number_a + number_b;
+        }*/
+
+        /*double i = 9.4;
+        System.out.println( Math.floor(i));*/
+
+//        float a = abs(-19.23f);
+//        System.out.println(a);
+//        System.out.println(abs(-324.324));
+
+//        String ch = "2345235";
+//        int a = Integer.parseInt(ch, 16);
+//        System.out.println( Integer.parseInt(ch));
+//        System.out.println(a);
+
+//        Float a = 233.23f;
+//        System.out.println( a.compareTo(233.23f) );
+
+//
+//        Float a = 130.324f;
+//
+//        System.out.println( a.doubleValue() );
+//        int a = floatValue(a);
+
+//        Character a = 'w';
+//
+////        char c = test('x');
+//        System.out.println(a);
+    //}
+
+
+
+   /* public static void main1(String[] args) {
+        int number_a = 10;
+        int number_b = 20;
+        int ret = sum(number_a, number_b);
+        System.out.println(ret);
+    }*/
+
+/*
+    *
+     * 该方法是用来求和，两个参数都是int类型
+     * @param number_a 第一个加数
+     * @param number_b 第二个加数
+     * @return 返回一个int类型的值；*/
+    /*public static int sum(int number_a, int number_b) {
+        return number_a + number_b;
+    }*/
+//}
+
