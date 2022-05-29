a41e7dd47795042e54c895673444a1c666f43f68
gitee_drh <2062806593@qq.com>
2022-05-29 22:11:43 +0800

    5_29的代码

---
+++ Java_5_29/src/com/company/Demo.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Demo {
+
+    public Demo() {
+        this(10);
+    }
+    public Demo(int x) {
+        System.out.println(x);
+    }
+}
---
+++ Java_5_29/src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {// 1
+    public static void main(String args[]) {
+        Demo demo = new Demo();
+
+    }
+}
+
+
+
