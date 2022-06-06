60c68f68f33a05f5afe00bde7de3b896ea3fcbe5
gitee_drh <2062806593@qq.com>
2022-06-06 21:22:27 +0800

    图书管理系统

--- LibraryManagementSystem/src/Main.java
+++ LibraryManagementSystem/src/Main.java
@@ -1,7 +1,14 @@
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("请输入你的用户名：");
+        String userName = scanner.nextLine();
+        System.out.println("hello " + userName + "请问你是1.管理员》 0.普通用户》");
+        int userChoose = scanner.nextInt();
+
 
     }
 }
