39a72f8e70b1177dab41debe6ebf79fff5cfde5d
gitee_drh <2062806593@qq.com>
2022-06-06 20:41:23 +0800

    图书管理系统

--- LibraryManagementSystem/src/user/User.java
+++ LibraryManagementSystem/src/user/User.java
@@ -1,5 +1,20 @@
 package user;
 
-public class User {
+import books.BookList;
+import operation.IOperation;
+
+public abstract class User {
+    protected String userName;
+    protected IOperation[] iOperations;
+
+    public User(String userName) {
+        this.userName = userName;
+    }
+
+    public abstract void menu();
+
+    public void doperation(int choose, BookList bookList) {
+        this.iOperations[choose].work(bookList);
+    }
 
 }
