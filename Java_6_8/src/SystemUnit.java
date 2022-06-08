1f887b284b10821047e6a4e9fbdab77abcbc7657
gitee_drh <2062806593@qq.com>
2022-06-08 21:39:25 +0800

    6_18

--- Java_6_8/src/SystemUnit.java
+++ Java_6_8/src/SystemUnit.java
@@ -1,5 +1,9 @@
 
 public class SystemUnit {
+
+
+    
+}
    /* public static boolean isAdmin(String userId){
         return userId.toLowerCase()=="admin";
     }
@@ -7,6 +11,7 @@ public class SystemUnit {
         System.out.println(isAdmin("Admin"));
         System.out.println("Admin".toLowerCase());
     }*/
+/*
 
     public static void main(String[] args) {
         String s1="abc"+"def";//1
@@ -15,5 +20,23 @@ public class SystemUnit {
             System.out.println(".equals succeeded");//4
         if(s1==s2)//5
             System.out.println("==succeeded");//6
+
     }
-}
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        scanner.close();
+
+        System.out.print(a + b + " ");
+        System.out.print( Math.abs(a - b) + " ");
+        System.out.print(a * b + " ");
+        if (a > b) {
+            System.out.print(a / b + " ");
+            System.out.print(a % b + " ");
+        } else {
+            System.out.print(b / a + " ");
+            System.out.print(b % a + " ");
+        }
+    }
+}*/
