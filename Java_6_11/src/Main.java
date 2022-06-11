01237522a522e99549c997345d4dd187aa0b3afa
gitee_drh <2062806593@qq.com>
2022-06-11 19:56:19 +0800

    6_11

---
+++ Java_6_11/src/Main.java
@@ -0,0 +1,22 @@
+
+public class Main {
+
+    public static<T> void fun (Object ch, int i) {
+        du<T> = ch;
+    }
+
+    public static void main(String[] args) {
+
+        String arr = "hello";
+
+        fun(arr, 0);
+
+        double a = 23.32;
+        fun(a, 1);
+
+        System.out.println(obj[0]);
+        System.out.println(obj[1]);
+
+    }
+
+}
