d4c7705c61a2cd76e62c83251438ff04bd519999
gitee_drh <2062806593@qq.com>
2022-06-12 20:34:52 +0800

    6_12

---
+++ Java_6_12/src/Main.java
@@ -0,0 +1,26 @@
+
+class Demo<T extends Comparable<T>> {
+
+    public T findMaxVal(T[] array) {
+        T tmp = array[0];
+
+        for (int i = 0; i < array.length; i++) {
+            if (tmp.compareTo(array[i]) < 0) {
+                tmp = array[i];
+            }
+        }
+
+        return tmp;
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Demo<Integer> test = new Demo();
+
+        Integer[] arr = {12, 32, 45, 67};
+        System.out.println(test.findMaxVal(arr));
+    }
+}
