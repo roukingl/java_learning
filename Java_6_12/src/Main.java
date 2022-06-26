63d6cc5a544b08444efc3495f322298a7d3cde42
gitee_drh <2062806593@qq.com>
2022-06-26 16:16:26 +0800

    6_12

--- Java_6_12/src/Main.java
+++ Java_6_12/src/Main.java
@@ -23,7 +23,7 @@ public class Main {
 
         Demo<Integer> test = new Demo();
 
-
+        System.out.println("hid");
 
         Integer[] arr = {12, 32, 45, 67};
         System.out.println(test.findMaxVal(arr));
