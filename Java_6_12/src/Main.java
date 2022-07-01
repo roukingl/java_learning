b287fcf79d2a41ea532d4d695a5e07229ab31bd2
gitee_drh <2062806593@qq.com>
2022-07-01 09:02:30 +0800

    6_12

--- Java_6_12/src/Main.java
+++ Java_6_12/src/Main.java
@@ -26,6 +26,7 @@ public class Main {
         System.out.println("hid");
         System.out.println("hid");
         System.out.println("hid");
+
         System.out.println("hid");
 
         Integer[] arr = {12, 32, 45, 67};
