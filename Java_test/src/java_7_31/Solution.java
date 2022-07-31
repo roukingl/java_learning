2f67981e2904a8fec3c08c5dad7abbbef82a6cf3
gitee_drh <2062806593@qq.com>
2022-07-31 22:26:31 +0800

    题

--- Java_test/src/java_7_31/Solution.java
+++ Java_test/src/java_7_31/Solution.java
@@ -39,12 +39,49 @@ public class Solution {
     }
 
     // nowcoder BM3 链表中的节点每k个一组翻转
-    public ListNode reverseKGroup (ListNode head, int k) {
+    /*public ListNode reverseKGroup (ListNode head, int k) {
         if (head == null) {
             return null;
         }
         ListNode node = new ListNode(-1);
         node.next = head;
+    }*/
+
+    //BM4 合并两个排序的链表
+    public ListNode Merge(ListNode list1,ListNode list2) {
+        if (list1 == null && list2 == null) {
+            return null;
+        }
+        if (list1 == null) {
+            return list2;
+        }
+        if (list2 == null) {
+            return list1;
+        }
+        ListNode cur1 = list1;
+        ListNode cur2 = list2;
+        while (cur1 != null && cur2 != null) {
+            if (cur1.val < cur2.val) {
+                while (cur1.next != null && cur1.next.val <= cur2.val) {
+                    cur1 = cur1.next;
+                }
+                ListNode cur1Next = cur1.next;
+                cur1.next = cur2;
+                cur1 = cur1Next;
+            } else {
+                while (cur2.next != null && cur2.next.val <= cur1.val) {
+                    cur2 = cur2.next;
+                }
+                ListNode cur2Next = cur2.next;
+                cur2.next = cur1;
+                cur2 = cur2Next;
+            }
+        }
+        if (list1.val < list2.val) {
+            return list1;
+        } else {
+            return list2;
+        }
     }
 }
 
