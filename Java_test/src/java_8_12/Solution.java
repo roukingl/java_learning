3a8d26c6a648eeb7c60405b2bca48e588631f230
gitee_drh <2062806593@qq.com>
2022-08-12 21:52:44 +0800

    题

---
+++ Java_test/src/java_8_12/Solution.java
@@ -0,0 +1,74 @@
+package java_8_12;
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Stack;
+
+class Solution {
+
+    // leetcode 98.验证二叉搜素树
+    // 递归
+    public boolean isValidBST1(TreeNode root) {
+        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    public boolean isValidBST(TreeNode root, long lower, long upper) {
+        if (root == null) {
+            return true;
+        }
+        if (root.val <= lower || root.val >= upper) {
+            return false;
+        }
+        return isValidBST(root.left, lower, root.val) && isValidBST(root.right, root.val, upper);
+    }
+
+    // 中序排列
+    public boolean isValidBST(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<>();
+        double inorder = -Double.MAX_VALUE;
+        while (!stack.isEmpty() || root != null) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            if (root.val <= inorder) {
+                return false;
+            }
+            inorder = root.val;
+            root = root.right;
+        }
+        return true;
+    }
+
+/*    private boolean judgeOneSideTree(TreeNode root, TreeNode node, boolean flg) {
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(node);
+        while (!stack.empty()) {
+            TreeNode cur = stack.pop();
+            if (flg && cur.val >= root.val) {
+                return false;
+            }
+            if (!flg && cur.val <= root.val) {
+                return false;
+            }
+            if (cur.left != null) {
+                stack.push(cur.left);
+            }
+            if (cur.right != null) {
+                stack.push(cur.right);
+            }
+        }
+        return true;
+    }*/
+}
+
+
+
+
+
+
+
+
+
+
