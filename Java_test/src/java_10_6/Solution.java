003228882dd304c2b30eac575000f4312249e5d1
gitee_drh <2062806593@qq.com>
2022-10-06 22:52:52 +0800

    题 and 部分算法实现

---
+++ Java_test/src/java_10_6/Solution.java
@@ -0,0 +1,28 @@
+package java_10_6;
+
+import java.lang.reflect.Array;
+import java.util.LinkedList;
+
+class Solution {
+
+    // leetcode 53. 最大子数组和
+    public int maxSubArray(int[] nums) {
+        int len = nums.length;
+        int[] dp = new int[len];
+        dp[0] = nums[0];
+        for (int i = 1; i < len; i++) {
+            if (dp[i - 1] > 0) {
+                dp[i] = dp[i - 1] + nums[i];
+            } else {
+                dp[i] = nums[i];
+            }
+        }
+        int result = Integer.MIN_VALUE;
+        for (int i = 0; i < len; i++) {
+            if (result < dp[i]) {
+                result = dp[i];
+            }
+        }
+        return result;
+    }
+}
