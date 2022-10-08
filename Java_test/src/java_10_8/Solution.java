018063b835b4dddbdc18c8d29479f4181d093018
gitee_drh <2062806593@qq.com>
2022-10-08 21:59:27 +0800

    题 and 部分算法实现

---
+++ Java_test/src/java_10_8/Solution.java
@@ -0,0 +1,31 @@
+package java_10_8;
+
+import java.util.*;
+
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        if (nums1.length > nums2.length) {
+            return intersect(nums2, nums1);
+        }
+        // K为值, V是个数
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int ret : nums1) {
+            map.put(ret, map.getOrDefault(ret, 0) + 1);
+        }
+        int index = 0;
+        int[] result = new int[nums1.length];
+        for (int ret : nums2) {
+            int count = map.getOrDefault(ret, 0);
+            if (count > 0) {
+                result[index++] = ret;
+                count--;
+                if (count > 0) {
+                    map.put(ret, count);
+                } else {
+                    map.remove(ret);
+                }
+            }
+        }
+        return Arrays.copyOfRange(result, 0, index);
+    }
+}
