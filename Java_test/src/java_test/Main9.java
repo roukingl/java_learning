ff21363223b41e12383df411fd8339810d26c4d5
gitee_drh <2062806593@qq.com>
2022-10-18 20:51:20 +0800

    题 and 部分算法实现

---
+++ Java_test/src/java_test/Main9.java
@@ -0,0 +1,11 @@
+package java_test;
+
+import java.util.Scanner;
+
+public class Main9 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
