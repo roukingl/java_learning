473872a5f43d1979a6b891ff21fcbbefa2faef44
RuiHangDing <2062806593@qq.com>
2022-11-02 23:40:48 +0800

    题 and 部分算法实现

--- Java_test/src/java_11_2/other/Solution.java
+++ Java_test/src/java_11_2/other/Solution.java
@@ -1,8 +1,7 @@
-package java_11_2;
+package java_11_2.other;
 
-import java.lang.reflect.Array;
-import java.util.Arrays;
 
+// leetcode 322.零钱兑换，不会
 class Solution {
     public static int coinChange1(int[] coins, int amount) {
         if (coins == null || coins.length == 0 || amount < 0) {
@@ -29,31 +28,70 @@ class Solution {
         return sum;
     }
 
+
+    // 记忆化搜索
     public static int coinChange2(int[] coins, int amount) {
         if (coins == null || coins.length == 0 || amount < 0) {
             return -1;
         }
         int len = coins.length;
         int[][] dp = new int[len + 1][amount + 1];
-        for (int i = 1; i < amount + 1; i++) {
-            dp[len][i] = -1;
+        for (int i = 0; i < len + 1; i++) {
+            for (int j = 0; j < amount + 1; j++) {
+                dp[i][j] = -2;
+            }
         }
         return process2(coins, 0, amount, dp);
     }
 
     private static int process2(int[] arr, int index, int amount, int[][] dp) {
-        if (dp[index][amount] != -1) {
+        if (dp[index][amount] != -2) {
+            return dp[index][amount];
+        }
+        if (index == arr.length) {
+            dp[index][amount] = amount == 0 ? 0 : -1;
             return dp[index][amount];
         }
         int sum = -1;
         for (int zhang = 0; arr[index] * zhang <= amount; zhang++) {
             int next = process2(arr, index + 1, amount - zhang * arr[index], dp);
-            dp[index + 1][amount - zhang * arr[index]] = (sum == -1 ? next + zhang : Math.min(next + zhang, sum));
             if (next != -1) {
-                sum = dp[index + 1][amount - zhang * arr[index]];
+                sum = sum == -1 ? next + zhang : Math.min(sum, next + zhang);
             }
         }
+        dp[index][amount] = sum;
         return sum;
+    }
+
+    // 动态规划
+    public static int coinChange3(int[] arr, int aim) {
+        if (arr == null || arr.length == 0 || aim < 0) {
+            return -1;
+        }
+        int N = arr.length;
+        int[][] dp = new int[N + 1][aim + 1];
+        // data base
+        for (int i = 1; i < aim + 1; i++) {
+            dp[N][i] = -1;
+        }
+
+        for (int i = N - 1; i >= 0; i--) {
+            for (int rest = 0; rest < aim + 1; rest++) {
+                dp[i][rest] = -1;
+                if (dp[i + 1][rest] != -1) {
+                    dp[i][rest] = dp[i + 1][rest];
+                }
+                if (rest - arr[i] >= 0 && dp[i][rest - arr[i]] != -1) {
+                    if (dp[i][rest] == -1) {
+                        // 下方的值无效，要抵消-1所以要额外加一
+                        dp[i][rest] = dp[i][rest - arr[i]] + 1;
+                    } else {
+                        dp[i][rest] = Math.min(dp[i][rest], dp[i][rest - arr[i]] + 1);
+                    }
+                }
+            }
+        }
+        return dp[0][aim];
     }
 
 
@@ -72,7 +110,7 @@ class Solution {
         for (int i = 0; i < testTime; i++) {
             int[] arr = generateRandomArray(len, max);
             int aim = (int) (Math.random() * 3 * max) + max;
-            if (coinChange1(arr, aim) != coinChange2(arr, aim)) {
+            if (coinChange1(arr, aim) != coinChange2(arr, aim) || coinChange3(arr, aim) != coinChange2(arr, aim)) {
                 System.out.println("ooops!");
                 break;
             }
