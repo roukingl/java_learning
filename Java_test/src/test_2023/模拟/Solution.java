466e192924100539d38665ad3e28876d42b71f0b
小疯不疯 <105028530+roukingl@users.noreply.github.com>
2024-01-08 23:53:18 +0800

    习题练习及部分算法实现

--- Java_test/src/test_2023/模拟/Solution.java
+++ Java_test/src/test_2023/模拟/Solution.java
@@ -3,7 +3,6 @@ package test_2023.模拟;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
-
 class Solution {
     public static void main(String[] args) {
         int[] tmp = new int[]{2, 0, 2, 1, 1, 0};
