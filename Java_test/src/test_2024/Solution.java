01251e9c3af3dc5ceb44494d942298ae46fa2faa
小疯不疯 <105028530+roukingl@users.noreply.github.com>
2024-03-12 23:49:26 +0800

    习题练习及部分算法实现

---
+++ Java_test/src/test_2024/Solution.java
@@ -0,0 +1,36 @@
+package test_2024;
+
+import java.util.Random;
+
+public class Solution {
+
+    private void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+    public int[] sortArray(int[] nums) {
+        sort(nums, 0, nums.length - 1);
+        return nums;
+    }
+
+    private void sort(int[] nums, int l, int r) {
+        if (l >= r) {
+            return;
+        }
+        int key = nums[new Random().nextInt(r - l + 1) + l];
+        int left = l - 1;
+        int right = r + 1;
+        for (int i = l; i < right;) {
+            if (nums[i] < key) {
+                swap(nums, ++left, i++);
+            } else if (nums[i] == key) {
+                i++;
+            } else {
+                swap(nums, --right, i);
+            }
+        }
+        sort(nums, l, left);
+        sort(nums, right, r);
+    }
+}
